29ad232de5aaa44e06c49b77db36e15fe40b9d4f
SAGARWORKLAPTOP\Sagar <sagar41196@gmail.com>
2017-12-07 20:47:50 -0500

    admin servlet done.

---
+++ src/servlets/AdminServlet.java
@@ -0,0 +1,60 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdminServlet
+ */
+@WebServlet("/AdminServlet")
+public class AdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		String inDate =  request.getParameter("inDate");
+		String outDate = request.getParameter("outDate");
+		SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd");
+		Date inD=null , inO=null;
+		try {
+			 inD= (Date) sdf.parse(inDate);
+			 inO=(Date) sdf.parse(outDate);
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		request.getRequestDispatcher("/index.jsp").forward(request,response);
+	
+	}
+
+}
